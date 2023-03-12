65109bdf4fee4f85b2759b7c92c0c691afa080e4
Nicholas_Drake <nicholasdrake6@gmail.com>
2023-03-12 13:26:48 -0500

    fixed spelling in app

--- app/pages/2_Info.py
+++ app/pages/2_Info.py
@@ -18,8 +18,8 @@ img_nd = Image.open("images/nd_2022.jpg").resize((400, 500))
 with st.container():
     st.title("Additional Information")
     st.markdown('''This project was built for MSDS 434 Analytics Application Development Final Project (Part II). The final project consists of building a cloud-native 
-    analytics application that is hosted on the Google Cloud Platform (GCP). The GitHub repository for this project with more technical documentation can be 
+    analytics application that is hosted on the Google Cloud Platform (GCP). The GitHub repository for this project with the technical documentation can be 
     [found here](https://github.com/DrakeData/MSDS434-Final_P2).''')
-    st.subheader("Project was creatd by Nicholas Drake")
+    st.subheader("Project was created by Nicholas Drake")
     st.image(img_nd)
     st.markdown("**Contact:** [GitHub](https://github.com/DrakeData) - [LinkedIn](https://www.linkedin.com/in/ndrakedata/) - [Email](mailto:nick@drakedata.io)")
